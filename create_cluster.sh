234b96907c5a05bdae0f866fdc6fdc70c98809d5
NarothamSai <narotham@buildstorm.com>
2020-09-02 20:08:14 +0530

    Add create kubernetes cluster script

---
+++ create_cluster.sh
@@ -0,0 +1,1 @@
+eksctl create cluster --name=nginx-web-server --version=1.17 --nodegroup-name=standard-workers --node-type=t2.medium --nodes=3 --nodes-min=1 --nodes-max=4 --node-ami=auto --region=ap-south-1 --node-volume-size=50
